74432e93a34e9b85d626f8b58fc976c9c689ed5f
rslavin <rocky.slavin@gmail.com>
2020-12-03 14:27:00 -0600

    refactored day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,13 +1,8 @@
-def count_trees(dx, dy, lines):
-    x = trees = 0
-    for line in lines[::dy]:
-        trees += 1 if line[x % len(line)] == "#" else 0
-        x += dx
-    return trees
+def count(dx, dy, lines):
+    return len([1 for n, l in enumerate(lines[::dy]) if l[n * dx % len(l)] == "#"])
 
 
 with open('day3.in') as f:
     lines = f.read().splitlines()
-print(count_trees(3, 1, lines))
-print(count_trees(1, 1, lines) * count_trees(3, 1, lines) *
-      count_trees(5, 1, lines) * count_trees(7, 1, lines) * count_trees(1, 2, lines))
+print(count(3, 1, lines))
+print(count(1, 1, lines) * count(3, 1, lines) * count(5, 1, lines) * count(7, 1, lines) * count(1, 2, lines))
