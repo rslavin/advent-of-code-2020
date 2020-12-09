a4b7cff9884629f0ea9fc80a27fb237c3b5dc771
rslavin <rocky.slavin@gmail.com>
2020-12-09 09:23:10 -0600

    day 9

---
+++ day9/day9.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+with open('day9.in') as f:
+    numbers = list(map(int, f.read().splitlines()))
+
+
+def find_invalid():
+    for i in range(25, len(numbers)):
+        if not [pair for pair in permutations(numbers[i - 25:i], 2) if sum(pair) == numbers[i]]:
+            return numbers[i]
+
+
+def find_sequence(total):
+    for i, n in enumerate(numbers):
+        for j, m in enumerate(numbers[i:]):
+            if sum(numbers[i:j]) == total:
+                return min(numbers[i:j]) + max(numbers[i:j])
+
+
+invalid = find_invalid()
+print(invalid)
+print(find_sequence(invalid))
